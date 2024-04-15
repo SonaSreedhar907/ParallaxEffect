00f16ae2f5e185ad5a7c61483634dc53298d3040
Sona Sreedhar <sonasreedhar44@gmail.com>
2024-04-15 20:18:49 +0530

    parallax effect

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import One from './components/One'
+import Two from './components/Two'
+import Three from './components/Three'
+import TextBox from './components/TextBox'
+function App() {
+  return (
+    <div>
+      <One/>
+      <TextBox/>
+      <Two/>
+      <TextBox/>
+      <Three/>
+    </div>
+  )
+}
+
+export default App
